c8a15ec79efd0ca7e6a1e825bb923b5810dcaf5e
aleksandr kryachko <aleksan.kryachko@gmail.com>
2022-11-22 08:20:49 +0700

    check main page

--- mainpage.py
+++ mainpage.py
@@ -2,26 +2,20 @@ import pytest
 import requests
 from selenium import webdriver
 import time
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-driver = webdriver.Chrome(executable_path="./chromedriver.exe", )
+
+# driver = webdriver.ChromeOptions()
 
 def test_01_status_code():
-    url = 'https://www.ozon.ru/'
+    url = 'http://www.python.org'
     responce = requests.get(url=url)
     assert responce.status_code == 200, 'Статус не 200'
+def test_02_main_page():
+    driver = webdriver.Chrome(executable_path=".chromedriver.exe")
+    driver = webdriver.Chrome()
 
-print('responce.status_code')
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    time.sleep(2)
+    driver.close()
 
-driver.get(url='https://www.ozon.ru/')
-#time.sleep(10)
-#driver.close()
-def __init__(self, browser, url, timeout=10):  # команду для неявного ожидания со значением по умолчанию в 10:
-    self.browser = browser
-    self.url = url
-    self.browser.implicitly_wait(timeout)
 
-def test_02_open_main_page(self):
-    self.browser.get(self.url)
-    time.sleep(10)
-# def test03_should_be_login_form(self):
