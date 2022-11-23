ca7c8d0e23575b71cd73d235099a985d8190e57b
aleksandr kryachko <aleksan.kryachko@gmail.com>
2022-11-23 12:18:41 +0700

    add check elements

--- mainpage.py
+++ mainpage.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+
 
 
 @pytest.fixture
@@ -20,17 +20,23 @@ def browser():
 def test_01_status_code():
     url = 'http://www.python.org'
     responce = requests.get(url=url)
-    assert responce.status_code == 200, 'Статус не 200'
+    assert responce.status_code == 200, 'status not 200'
 def test_02_main_page(browser):
 
-    assert "Python" in browser.title, 'не титульная страница'
+    assert "Python" in browser.title, 'titel page'
 
 def test_03_assert_button(browser):
 
-    assert browser.find_elements(By.XPATH, '//*[@id="id-search-field"]'), 'нет поиска'
-    assert browser.find_element(By.CSS_SELECTOR, '#downloads > a'), 'нет Загрузить'
-    assert browser.find_element(By.NAME, 'q'), 'поисковая строка'
-    assert browser.find_element(By.ID, 'submit'), 'кнопки найти'
+    assert browser.find_elements(By.XPATH, '//*[@id="id-search-field"]'), 'Search'
+    assert browser.find_elements(By.XPATH, '//*[@id="news"]/a'), 'button News'
+    assert browser.find_element(By.CSS_SELECTOR, '#downloads > a'), 'Download'
+    assert browser.find_element(By.CSS_SELECTOR, "#documentation > a"), 'Button community'
+    assert browser.find_element(By.NAME, 'q'), 'Search'
+    assert browser.find_element(By.ID, 'submit'), 'button Go'
+    assert browser.find_element(By.CLASS_NAME, 'container'), 'container test python'
+    assert browser.find_element(By.CLASS_NAME, 'site-headline'), 'logo python tm'
+
+
 
 
 
