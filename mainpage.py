5be1cc15c1a20dd97b9de2a00c17880fcbf17348
aleksandr kryachko <aleksan.kryachko@gmail.com>
2022-11-23 11:58:20 +0700

    добавил fixture

--- mainpage.py
+++ mainpage.py
@@ -4,31 +4,34 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-url = 'http://www.python.org'
 
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    url = 'http://www.python.org'
+    browser = webdriver.Chrome(executable_path=".chromedriver.exe")
+    browser = webdriver.Chrome()
+    browser.get(url=url)
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    browser.quit()
 
 def test_01_status_code():
+    url = 'http://www.python.org'
     responce = requests.get(url=url)
     assert responce.status_code == 200, 'Статус не 200'
-def test_02_main_page():
-    url = 'http://www.python.org'
-    driver = webdriver.Chrome(executable_path=".chromedriver.exe")
-    driver = webdriver.Chrome()
+def test_02_main_page(browser):
 
-    driver.get(url=url)
-    assert "Python" in driver.title,'не титульная страница'
-    time.sleep(2),
+    assert "Python" in browser.title, 'не титульная страница'
 
-def test_03_assert_button():
-    driver = webdriver.Chrome(executable_path=".chromedriver.exe")
-    driver = webdriver.Chrome()
-    driver.get(url=url)
-    assert driver.find_elements(By.XPATH, '//*[@id="id-search-field"]'), 'нет поиска'
-    assert driver.find_element(By.CSS_SELECTOR, '#downloads > a'), 'нет Загрузить'
-    assert driver.find_element(By.NAME, 'q'), 'поисковая строка'
-    assert driver.find_element(By.ID, 'submit'), 'кнопки найти'
-    time.sleep(1),
-    driver.close()
+def test_03_assert_button(browser):
+
+    assert browser.find_elements(By.XPATH, '//*[@id="id-search-field"]'), 'нет поиска'
+    assert browser.find_element(By.CSS_SELECTOR, '#downloads > a'), 'нет Загрузить'
+    assert browser.find_element(By.NAME, 'q'), 'поисковая строка'
+    assert browser.find_element(By.ID, 'submit'), 'кнопки найти'
+
 
 
 
