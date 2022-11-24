bb252d436c78e156fd5d23cf19d5b29f1c5d4583
aleksandr kryachko <aleksan.kryachko@gmail.com>
2022-11-24 20:36:12 +0700

    add check search 1 step

--- mainpage.py
+++ mainpage.py
@@ -1,16 +1,21 @@
 import pytest
 import requests
+import time
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
+url = 'http://www.python.org'
 
 @pytest.fixture
 def browser():
     print("\nstart browser for test..")
-    url = 'http://www.python.org'
+
     browser = webdriver.Chrome(executable_path=".chromedriver.exe")
     browser = webdriver.Chrome()
+    browser.set_window_size(1416, 1026)
+    # browser.maximize_window()
+    print(browser.get_window_size())
     browser.get(url=url)
     yield browser
     # этот код выполнится после завершения теста
@@ -23,7 +28,7 @@ def test_01_status_code():
     assert responce.status_code == 200, 'status not 200'
 def test_02_main_page(browser):
 
-    assert "Python" in browser.title, 'titel page'
+    assert "Python" in browser.title, 'title page'
 
 def test_03_assert_button(browser):
 
@@ -35,6 +40,31 @@ def test_03_assert_button(browser):
     assert browser.find_element(By.ID, 'submit'), 'button Go'
     assert browser.find_element(By.CLASS_NAME, 'container'), 'container test python'
     assert browser.find_element(By.CLASS_NAME, 'site-headline'), 'logo python tm'
+
+def test_04_check_search(browser):
+
+    browser.find_element(By.NAME, 'q').clear()
+    browser.find_element(By.NAME, 'q').send_keys('3.9')
+    browser.find_element(By.ID, 'submit').click()
+    # time.sleep(5)
+    assert browser.find_element(By.CLASS_NAME, 'site-headline'), 'logo python tm'
+    assert browser.find_elements(By.CSS_SELECTOR, '#content > div > section > h2'), 'Search'
+    assert browser.find_element(By.NAME, 'q'), 'Search'
+
+    browser.find_element(By.NAME, 'q').clear()
+    browser.find_element(By.NAME, 'q').send_keys('python')
+    browser.find_element(By.ID, 'submit').send_keys(Keys.ENTER)
+
+    assert browser.find_elements(By.XPATH, '//*[@id="news"]/a'),'block_News'
+    assert browser.find_elements(By.CSS_SELECTOR, '#content > div > section > form > h3'), 'block_world_Result'
+    assert browser.find_elements(By.CSS_SELECTOR, '#content > div > section > form > ul'), 'url_download'
+
+    browser.find_element(By.NAME, 'q').clear()
+    browser.find_element(By.NAME, 'q').send_keys('5682146212221')
+    browser.find_element(By.ID, 'submit').send_keys(Keys.ENTER)
+    assert browser.find_elements(By.CSS_SELECTOR, '#content > div > section > form > ul > p'), 'Not found'
+
+
 
 
 
