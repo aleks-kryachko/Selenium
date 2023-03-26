dc90e651b26aace17edfc6631451711d7fd9531b
aleksandr kryachko <aleksan.kryachko@gmail.com>
2023-03-26 15:24:56 +0700

    yandex test

--- tests/test_Yandex.py
+++ tests/test_Yandex.py
@@ -9,6 +9,6 @@ binary_yandex_driver_file = '.yandexdriver.exe' #for windows
 yandex_browser_package_name = 'com.yandex.browser' # Release version of Yandex.Browser
 driver = webdriver.Chrome(binary_yandex_driver_file, options=options)
 driver.get('https://yandex.ru')
-driver.set_page_load_timeout(10) # ждем 10 пока прогрузится страница или найдется элемент
-time.sleep(1)
-driver.quit()
+driver.set_page_load_timeout(10) # ждем 10 сек. пока прогрузится страница или найдется элемент
+time.sleep(10)
+# driver.quit()
